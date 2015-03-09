49b303b6c0225d581de3041bdeea507815ebe685
DQNEO <dqneoo@gmail.com>
2015-03-10 00:23:00 +0900

    set modifier

--- Acme/FizzBuzz.php
+++ Acme/FizzBuzz.php
@@ -3,7 +3,7 @@ namespace Acme;
 
 class FizzBuzz
 {
-    function main($max)
+    public function main($max)
     {
         foreach (range(1,$max) as $n) {
             if ($n % 3 === 0 && $n % 5 === 0) {
@@ -20,7 +20,7 @@ class FizzBuzz
         }
     }
 
-    function output($string)
+    public function output($string)
     {
         echo $string;
     }
