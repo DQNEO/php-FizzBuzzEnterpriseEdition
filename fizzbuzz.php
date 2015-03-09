f6b0d9ebc51d932ac5eb27faee37d0a348af416e
DQNEO <dqneoo@gmail.com>
2015-03-09 23:55:50 +0900

    FizzBuzzon mod 15

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -2,7 +2,9 @@
 <?php
 
 foreach (range(1,30) as $n) {
-    if ($n % 3 === 0) {
+    if ($n % 15 === 0) {
+        echo "FizzBuzz\n";
+    } else if ($n % 3 === 0) {
         echo "Fizz\n";
     } else if ($n % 5 === 0) {
         echo "Buzz\n";
