f783be5c238466b56163911b8973fc260445e544
DQNEO <dqneoo@gmail.com>
2015-03-10 00:11:26 +0900

    wrap by main function

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,16 +1,20 @@
 #!/usr/bin/env php
 <?php
 
-$max = $argv[1];
-foreach (range(1,$max) as $n) {
-    if ($n % 3 === 0 && $n % 5 === 0) {
-        echo "FizzBuzz\n";
-    } else if ($n % 3 === 0) {
-        echo "Fizz\n";
-    } else if ($n % 5 === 0) {
-        echo "Buzz\n";
-    } else {
-        echo "$n\n";
-    }
+main($argv[1]);
 
+function main($max)
+{
+    foreach (range(1,$max) as $n) {
+        if ($n % 3 === 0 && $n % 5 === 0) {
+            echo "FizzBuzz\n";
+        } else if ($n % 3 === 0) {
+            echo "Fizz\n";
+        } else if ($n % 5 === 0) {
+            echo "Buzz\n";
+        } else {
+            echo "$n\n";
+        }
+
+    }
 }
