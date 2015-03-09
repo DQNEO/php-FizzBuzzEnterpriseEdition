2b4d15fee8e84cdc9f5d5471ef71d9c76bbd269c
DQNEO <dqneoo@gmail.com>
2015-03-10 00:15:12 +0900

    separate file

---
+++ FizzBuzz.php
@@ -0,0 +1,19 @@
+<?php
+class FizzBuzz
+{
+    function main($max)
+    {
+        foreach (range(1,$max) as $n) {
+            if ($n % 3 === 0 && $n % 5 === 0) {
+                echo "FizzBuzz\n";
+            } else if ($n % 3 === 0) {
+                echo "Fizz\n";
+            } else if ($n % 5 === 0) {
+                echo "Buzz\n";
+            } else {
+                echo "$n\n";
+            }
+
+        }
+    }
+}
--- run.php
+++ run.php
@@ -1,23 +1,5 @@
 #!/usr/bin/env php
 <?php
+require_once __DIR__ . '/FizzBuzz.php';
 
 FizzBuzz::main($argv[1]);
-
-class FizzBuzz
-{
-    function main($max)
-    {
-        foreach (range(1,$max) as $n) {
-            if ($n % 3 === 0 && $n % 5 === 0) {
-                echo "FizzBuzz\n";
-            } else if ($n % 3 === 0) {
-                echo "Fizz\n";
-            } else if ($n % 5 === 0) {
-                echo "Buzz\n";
-            } else {
-                echo "$n\n";
-            }
-
-        }
-    }
-}
