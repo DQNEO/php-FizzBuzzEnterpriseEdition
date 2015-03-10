ba923f856f5a8cfa88ddbe72a5e3cabfa477db47
DQNEO <dqneoo@gmail.com>
2015-03-10 22:46:24 +0900

    garbage

--- autoload.php
+++ autoload.php
@@ -1,7 +1,4 @@
 <?php
-//require_once __DIR__ . '/Acme/FizzBuzz.php';
-//require_once __DIR__ . '/Acme/Writer.php';
-
 spl_autoload_register(function($class){
                           $slashed = strtr($class, '\\', '/');
                           require_once __DIR__ . '/' . $slashed . '.php';
