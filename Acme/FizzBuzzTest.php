b43096eed185138fea113354f7fe99a1c2ab039b
DQNEO <dqneoo@gmail.com>
2015-03-10 21:06:46 +0900

    test first that fails

--- Acme/FizzBuzzTest.php
+++ Acme/FizzBuzzTest.php
@@ -9,4 +9,11 @@ class FizzBuzzTest extends \PHPUnit_Framework_TestCase
         $fb = new FizzBuzz(1,1,1);
         $this->assertInstanceOf("Acme\\FizzBuzz", $fb);
     }
+
+    public function testRun1()
+    {
+        $fb = new FizzBuzz(1,3,5);
+        $ret = $fb->run(1);
+        $this->assertEquals("1\n", $ret);
+    }
 }
