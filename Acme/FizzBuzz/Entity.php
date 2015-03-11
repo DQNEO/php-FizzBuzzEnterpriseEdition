316f739ca23aa8bddee0d045b4354342a572e7be
DQNEO <dqneoo@gmail.com>
2015-03-11 23:34:46 +0900

    separate class files

---
+++ Acme/FizzBuzz/Entity.php
@@ -0,0 +1,18 @@
+<?php
+namespace Acme\FizzBuzz;
+
+class Entity
+{
+    private $value;
+
+    public function __construct($value)
+    {
+        $this->value = $value;
+    }
+
+    public function getValue()
+    {
+        return $this->value;
+    }
+}
+
--- Acme/FizzBuzz/FizzBuzz.php
+++ Acme/FizzBuzz/FizzBuzz.php
@@ -3,21 +3,6 @@ namespace Acme\FizzBuzz;
 use Acme\FizzBuzz\WriterInterface;
 use Acme\FizzBuzz\Entity;
 
-class Entity
-{
-    private $value;
-
-    public function __construct($value)
-    {
-        $this->value = $value;
-    }
-
-    public function getValue()
-    {
-        return $this->value;
-    }
-}
-
 class FizzBuzz
 {
     private $start;
