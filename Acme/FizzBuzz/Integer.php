58eff377ae49dfaef1a6603e66af4ab444817b24
DQNEO <dqneoo@gmail.com>
2015-03-12 00:53:47 +0900

    extract parent class

--- Acme/FizzBuzz/Integer.php
+++ Acme/FizzBuzz/Integer.php
@@ -1,18 +1,13 @@
 <?php
 namespace Acme\FizzBuzz;
 
-class Divident extends Integer
+class Integer
 {
     private $value;
 
     public function __construct($value)
     {
         $this->value = $value;
-    }
-
-    public function isDividableBy($divisor)
-    {
-        return ($this->value % $divisor === 0);
     }
 
     public function getValue()
