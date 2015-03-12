0c456879c5829bc258f3aff4324436356ba1dce9
DQNEO <dqneoo@gmail.com>
2015-03-13 01:46:13 +0900

    use factory

--- bin/run.php
+++ bin/run.php
@@ -3,12 +3,10 @@
 require_once __DIR__ . '/../autoload.php';
 use Acme\FizzBuzz\FizzBuzzRunner;
 use Acme\FizzBuzz\Writer;
-use Acme\FizzBuzz\RangeIterator;
+use Acme\FizzBuzz\RangeIteratorFactory;
 use Acme\FizzBuzz\DataType\Integer;
 
 $writer = new Writer;
 $fizzbuzz = new FizzBuzzRunner(3, 5, $writer);
-$min = new Integer(1);
-$max = new Integer($argv[1]);
-$range = new RangeIterator($min, $max);
+$range = RangeIteratorFactory::factory(1, $argv[1]);
 $fizzbuzz->run($range);
