923de9eb34e8b2bc73b3b6993aab0fc5ca5c85c4
DQNEO <dqneoo@gmail.com>
2015-03-17 21:14:26 +0900

    extract CLI gateway class

--- bin/run.php
+++ bin/run.php
@@ -1,18 +1,6 @@
 #!/usr/bin/env php
 <?php
 require_once __DIR__ . '/../autoload.php';
-use DQNEO\FizzBuzzEnterpriseEdition\FizzBuzzRunner;
-use DQNEO\FizzBuzzEnterpriseEdition\Writer;
-use DQNEO\FizzBuzzEnterpriseEdition\RangeIteratorFactory;
-use DQNEO\FizzBuzzEnterpriseEdition\DataType\Integer;
+use DQNEO\FizzBuzzEnterpriseEdition\CLI;
 
-$writer = new Writer;
-$fizzbuzz = new FizzBuzzRunner(new Integer(3), new Integer(5), $writer);
-
-if ($argc <= 1) {
-    echo("invalid argument\n");
-    exit;
-}
-
-$range = RangeIteratorFactory::factory(1, $argv[1]);
-$fizzbuzz->run($range);
+CLI::main($argc, $argv);
