6f3eba1fdc5b9c235f1ff6c1930d913193945d19
DQNEO <dqneoo@gmail.com>
2015-03-18 21:18:50 +0900

    add docblock

--- src/Entity/Number.php
+++ src/Entity/Number.php
@@ -4,6 +4,10 @@ use DQNEO\FizzBuzzEnterpriseEdition\DataType\Integer;
 
 class Number extends AbstractEntity
 {
+
+    /**
+     * @param Integer $object
+     */
     public function __construct(Integer $object)
     {
         $this->object = $object;
