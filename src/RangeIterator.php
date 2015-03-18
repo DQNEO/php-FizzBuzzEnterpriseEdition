7f0cae23b310377f75cce062468da2481c3a69d6
DQNEO <dqneoo@gmail.com>
2015-03-18 21:21:32 +0900

    add docblock

--- src/RangeIterator.php
+++ src/RangeIterator.php
@@ -9,6 +9,10 @@ class RangeIterator implements \Iterator
     private $end;
     private $current;
 
+    /**
+     * @param  Integer  $start
+     * @param  Integer  $end
+     */
     public function __construct(Integer $start, Integer $end)
     {
         $this->start = $start;
@@ -16,26 +20,41 @@ class RangeIterator implements \Iterator
         $this->current = $this->start->getValue();
     }
 
+    /**
+     * @return integer
+     */
     public function rewind()
     {
         $this->current = $this->start->getValue();
     }
 
+    /**
+     * @return integer
+     */
     public function next()
     {
         $this->current++;
     }
 
+    /**
+     * @return integer
+     */
     public function key()
     {
         return $this->current;
     }
 
+    /**
+     * @return Integer
+     */
     public function current()
     {
         return new Integer($this->current);
     }
 
+    /**
+     * @return boolean
+     */
     public function valid()
     {
         return $this->current <= $this->end->getValue();
