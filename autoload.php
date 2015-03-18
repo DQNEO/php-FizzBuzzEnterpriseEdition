c5828a01a6e1c15241ecaee4ccb45e2e75e4210e
DQNEO <dqneoo@gmail.com>
2015-03-18 21:55:48 +0900

    delegate autoloading to vendor/autoload.php

--- autoload.php
+++
@@ -1,7 +0,0 @@
-<?php
-spl_autoload_register(function ($class) {
-                          $elememts = explode('\\', $class);
-                          array_shift($elememts);
-                          array_shift($elememts);
-                          require_once __DIR__ . '/src/' . join('/', $elememts) . '.php';
-                      });
--- bin/fizzbuzz.php
+++ bin/fizzbuzz.php
@@ -1,6 +1,6 @@
 #!/usr/bin/env php
 <?php
-require_once __DIR__ . '/../autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 use DQNEO\FizzBuzzEnterpriseEdition\CLI;
 
 $status = CLI::main($argc, $argv);
