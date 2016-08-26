5c076f62599962ba4e3f5715d2cf07135a4ce30b
DQNEO <dqneoo@gmail.com>
2016-08-26 22:49:20 +0900

    readable

--- src/CLI.php
+++ src/CLI.php
@@ -10,14 +10,13 @@ class CLI
 {
     public static function main(int $argc, array $argv): int
     {
-        $writer = new StdoutWriter;
-        $fizzbuzz = new FizzBuzzApplication(new IntegerEntity(3), new IntegerEntity(5), $writer);
-
         if ($argc <= 1) {
             echo("invalid argument\n");
             return 1;
         }
 
+        $writer = new StdoutWriter;
+        $fizzbuzz = new FizzBuzzApplication(new IntegerEntity(3), new IntegerEntity(5), $writer);
         $range = RangeIteratorFactory::factory(1, $argv[1]);
         $fizzbuzz->run($range);
 
