645ed533f4b24207ef67f1bd604e38860e1185a9
DQNEO <dqneoo@gmail.com>
2016-08-26 22:41:38 +0900

    fix

--- test/test.sh
+++ test/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd $(dirname $0)
 cd ..
-diff <(php ./bin/run.php 30) ./test/result30.txt
+diff <(./bin/fizzbuzz.php 30) ./test/result30.txt
 if [ $? -eq 0 ]; then
     echo ok
 else
@@ -9,7 +9,7 @@ else
     exit 1
 fi
 
-diff <(php ./bin/run.php 100) ./test/result100.txt
+diff <(./bin/fizzbuzz.php 100) ./test/result100.txt
 if [ $? -eq 0 ]; then
     echo ok
 else
