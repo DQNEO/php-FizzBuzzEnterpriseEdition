eb3d110412a9daddde996af93791cf49db84f7c3
DQNEO <dqneoo@gmail.com>
2016-09-02 22:28:03 +0900

    add string value tests

---
+++ tests/Value/StringValueTest.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: DQNEO
+ * Date: 2016/09/02
+ * Time: 22:26
+ */
+
+namespace DQNEO\FizzBuzzEnterpriseEdition\Value;
+
+
+class StringValueTest extends \PHPUnit_Framework_TestCase
+{
+    public function testStringValue()
+    {
+        $obj1 = new StringValue('hello');
+        $this->assertEquals('hello', $obj1->getValue());
+
+        $obj2 = new StringValue(' world');
+        $this->assertEquals(' world', $obj2->getValue());
+    }
+
+}
