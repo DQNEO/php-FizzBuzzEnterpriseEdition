e7c669ccf9861c4cd695e30d2c5146eddc64a8d9
DQNEO <dqneoo@gmail.com>
2016-09-07 17:01:53 +0900

    deprecated

--- bin/fizzbuzz.php
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env php
-<?php
-require_once __DIR__ . '/../vendor/autoload.php';
-use DQNEO\FizzBuzzEnterpriseEdition\Console\Command;
-
-$status = Command::main($argc, $argv);
-exit($status);
